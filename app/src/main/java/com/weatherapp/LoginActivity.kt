f601ec7e9886c6c23cc1d11fc1ad2b07b85962f1
Samuel Lopes <samuelvitorprofissional@gmail.com>
2025-06-20 19:08:29 -0300

    PRÁTICA 1 – PARTE 3

--- app/src/main/java/com/weatherapp/LoginActivity.kt
+++ app/src/main/java/com/weatherapp/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.weatherapp
 
 import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -46,6 +48,7 @@ class LoginActivity : ComponentActivity() {
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun LoginPage(modifier: Modifier = Modifier) {
@@ -55,7 +58,9 @@ fun LoginPage(modifier: Modifier = Modifier) {
 
 
     Column(
-        modifier = modifier.padding(16.dp).fillMaxSize(),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = CenterHorizontally,
     ) {
@@ -87,9 +92,15 @@ fun LoginPage(modifier: Modifier = Modifier) {
             Button(
                 onClick = {
                     Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+                    activity?.startActivity(
+                        Intent(activity, MainActivity::class.java).setFlags(
+                            FLAG_ACTIVITY_SINGLE_TOP
+                        )
+                    )
                 },
-                enabled = email.isNotEmpty() && password.isNotEmpty()
-            ) {
+                enabled = email.isNotEmpty() && password.isNotEmpty(),
+
+                ) {
                 Text("Login")
             }
             Button(
--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.weatherapp
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +32,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HomePage(modifier: Modifier = Modifier) {
+    val activity = LocalContext.current as? Activity
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo à Home!",
+            fontSize = 26.sp
+        )
+
+        Spacer(modifier = Modifier.size(32.dp))
+
+        Button(
+            onClick = {
+                Toast.makeText(activity, "Saindo...", Toast.LENGTH_SHORT).show()
+                activity?.finish()
+            }
+        ) {
+            Text("Sair")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePagePreview() {
     WeatherAppTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
