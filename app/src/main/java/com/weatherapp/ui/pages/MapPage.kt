3f08d260b557ec6836549303118b1b25459d89f0
Samuel Lopes <samuelvitorprofissional@gmail.com>
2025-08-29 18:23:42 -0300

    PRÁTICA 4 – PARTE 4

--- app/src/main/java/com/weatherapp/ui/pages/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/pages/MapPage.kt
@@ -1,15 +1,27 @@
 package com.weatherapp.ui.pages
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.weatherapp.model.MainViewModel
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.weatherapp.R
+
 
 @Composable
 fun MapPage(viewModel: MainViewModel) {
@@ -18,12 +30,27 @@ fun MapPage(viewModel: MainViewModel) {
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
     val camPosState = rememberCameraPositionState()
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
+    val mapStyle = MapStyleOptions.loadRawResourceStyle(context, R.raw.map_night_style)
+
 
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(), onMapClick = {
             viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
-        }, cameraPositionState = camPosState
+        }, cameraPositionState = camPosState,
+        properties = MapProperties(
+            isMyLocationEnabled = hasLocationPermission,
+            mapStyleOptions = mapStyle
+        ),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
     ) {
 
         viewModel.cities.forEach {
