4311fb04ff8c3dc0c9f9dd1783c69a5c22bb46aa
Samuel Lopes <samuelvitorprofissional@gmail.com>
2025-08-29 18:11:03 -0300

    PRÁTICA 4 – PARTE 3

--- app/src/main/java/com/weatherapp/ui/pages/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/pages/MapPage.kt
@@ -1,36 +1,66 @@
 package com.weatherapp.ui.pages
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.weatherapp.model.MainViewModel
 
 @Composable
 fun MapPage(viewModel: MainViewModel) {
     val cityList = viewModel.cities
+    val recife = LatLng(-8.05, -34.9)
+    val caruaru = LatLng(-8.27, -35.98)
+    val joaopessoa = LatLng(-7.12, -34.84)
+    val camPosState = rememberCameraPositionState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Blue)
-            .wrapContentSize(Alignment.Center)
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(), onMapClick = {
+            viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
+        }, cameraPositionState = camPosState
     ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker(
+                    state = MarkerState(position = it.location),
+                    title = it.name, snippet = "${it.location}"
+                )
+            }
+        }
+
+        Marker(
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_RED
+            )
         )
+        Marker(
+            state = MarkerState(position = caruaru),
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_BLUE
+            )
+        )
+        Marker(
+            state = MarkerState(position = joaopessoa),
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_GREEN
+            )
+        )
+
+
     }
+
 }
